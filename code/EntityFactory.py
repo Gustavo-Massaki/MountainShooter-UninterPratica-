b63791d6f109f6c79d8360fecd9a6d5bc185dab1
GustavoYamamoto <yamamoto.pro.com@gmail.com>
2026-03-04 11:25:06 -0300

    AulaPratica4 Implementação 1 e 2 Players Inimigos

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,5 +1,9 @@
+import random
+
 from code.Background import Background
-from code.Const import WIN_WIDTH
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Enemy import Enemy
+from code.Player import Player
 
 
 class EntityFactory:
@@ -8,8 +12,16 @@ class EntityFactory:
     def get_entity(entity_name: str, position = (0,0)):
         match entity_name:
             case 'Level1Bg':
-                list_Bg = []
+                list_bg = []
                 for i in range(7):
-                    list_Bg.append(Background(f'Level1Bg{i}', (0, 0)))
-                    list_Bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
-                return list_Bg
+                    list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
+                return list_bg
+            case 'Player1':
+                return Player('Player1',(10, WIN_HEIGHT / 2))
+            case 'Player2':
+                return Player('Player2', (10, WIN_HEIGHT / 2 + 30))
+            case 'Enemy1':
+                return Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(40,WIN_HEIGHT - 40)))
+            case 'Enemy2':
+                return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(40,WIN_HEIGHT - 40)))
