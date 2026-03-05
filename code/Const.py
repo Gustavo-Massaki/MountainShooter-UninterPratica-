270294e32081fa494146460b330d3cbcd2fc1a6e
GustavoYamamoto <yamamoto.pro.com@gmail.com>
2026-03-05 15:49:17 -0300

    AulaPratica6 HUD e Score

--- code/Const.py
+++ code/Const.py
@@ -1,12 +1,32 @@
-#C
-
 import pygame
+
+#C
 
 C_ORANGE = (255,128,0)
 C_WHITE = (255,255,255)
 C_YELLOW = (0,0,0)
+C_GREEN = (0,128,0)
+C_CYAN = (0,128,128)
 
 #E
+
+ENTITY_DAMAGE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Player1': 1,
+    'Player2': 1,
+    'Player1Shot': 25,
+    'Player2Shot': 20,
+    'Enemy1': 1,
+    'Enemy2': 1,
+    'Enemy1Shot': 20,
+    'Enemy2Shot': 15,
+}
 
 ENTITY_HEALTH = {
     'Level1Bg0': 999,
@@ -24,6 +44,24 @@ ENTITY_HEALTH = {
     'Enemy2': 60,
     'Enemy1Shot': 1,
     'Enemy2Shot': 1,
+}
+
+ENTITY_SCORE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Player1': 0,
+    'Player2': 0,
+    'Player1Shot': 0,
+    'Player2Shot': 0,
+    'Enemy1': 100,
+    'Enemy2': 100,
+    'Enemy1Shot': 0,
+    'Enemy2Shot': 0,
 }
 
 ENTITY_SHOT_DELAY = {
